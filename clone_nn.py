ed0d9eb1bbeb024491c43fe028aea31d772ffb3d
Klaus Ening <nng@streetscooter.eu>
2018-03-08 15:58:16 +0100

    good working very simple solution

--- clone_nn.py
+++ clone_nn.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Flatten,Dense,Lambda
+from keras.layers import Flatten,Dense,Lambda,Convolution2D,MaxPool2D,Cropping2D,Dropout
 from keras.optimizers import Adam
 
 def load_data(files,brelocate = False, sNewDir=''):
@@ -20,7 +20,7 @@ def load_data(files,brelocate = False, sNewDir=''):
     images = []
     measurements = []
 
-    # todo: load left and r and offset the angle!
+    # todo: load left and r and offset the angle?
     for line in lines:
 
         angle = 0
@@ -30,36 +30,85 @@ def load_data(files,brelocate = False, sNewDir=''):
             continue
 
         src = line[0]
-        if brelocate:
-            fn = src.split('/')[-1]
-            src = sNewDir+fn
-        img=cv2.imread(src)
+
+        try: # allow to just delete bad images in the directory
+            if brelocate:
+                fn = src.split('/')[-1]
+                src = sNewDir+fn
+            img=cv2.imread(src)
+
+            img=cv2.resize(img,(0,0),fx=0.5, fy=0.5) # quater pixels for faster learning!
+
+            img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+
+            #cv2.cvtColor(center_image, cv2.COLOR_BGR2YUV)
+        except:
+            continue
 
 
         images.append(img)
 
         measurements.append(angle)
 
-    return (np.array(images), np.array(measurements))
+
+    return ([images, measurements])
+
+def enhance(img,ang):
+    # nothing yet
+    new_img = []
+    new_ang = []
+    for i in range(len(ang)):
+        new_ang.append(ang[i]*-1)
+        new_img.append(cv2.flip(img[i],1))
+
+    img.extend(new_img)
+    ang.extend(new_ang)
+    return [img,ang]
+
+def numpyfy(a):
+    return np.array(a)
 
 def get_nn():
     model = Sequential()
-    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3))) #normalize
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(80,160,3))) #normalize
+
+    model.add(Cropping2D(cropping=((36,13),(0,0)))) # crop to road
+
+
+    model.add(Convolution2D(24, (5, 5), strides=(2, 2), activation="relu"))
+    #model.add(Convolution2D(36, (5, 5), strides=(2, 2), activation="relu"))  # drop every second layer, for faster learning
+    model.add(Convolution2D(48, (5, 5), strides=(2, 2), activation="relu"))
+
+    #model.add(Convolution2D(64, (3, 3), activation="relu"))
+    model.add(Convolution2D(64, (3, 3), activation="relu"))
+
+    model.add(Dropout(0.4))
+
     model.add(Flatten())
+
+    model.add(Dense(100))
+    #model.add(Dense(50))
+    model.add(Dropout(0.4))
+
+    model.add(Dense(10))
+
     model.add(Dense(1))
     return model
 
-
-
-def train_nn(mod,X,y,valSplit=0.2,epochs=5, rate=0.001):
+def train_nn(mod,X,y,valSplit=0.2,epochs=2, rate=0.001):
     mod.compile(loss='mse', optimizer=Adam(lr=rate))
     mod.fit(X,y,validation_split=valSplit,shuffle=True,epochs=epochs)
     return mod
 
 def main():
-
-    X,y = load_data(['./recordings/001/driving_log.csv','./recordings/002/driving_log.csv'])
+    X,y = load_data(['./recordings/001/driving_log.csv','./recordings/002/driving_log.csv','./recordings/003/driving_log.csv'])
+    #X,y = enhance(X,y)
     nn = get_nn()
+
+    X=numpyfy(X)
+    y = numpyfy(y)
+
     train_nn(nn,X,y)
     nn.save('model.h5')
 
--- drive.py
+++ drive.py
@@ -60,8 +60,16 @@ def telemetry(sid, data):
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
+
+        # half!
+        image.thumbnail((160,320), Image.ANTIALIAS)
+
+
         image_array = np.asarray(image)
+
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+
+        steering_angle *=2
 
         throttle = controller.update(float(speed))
 
